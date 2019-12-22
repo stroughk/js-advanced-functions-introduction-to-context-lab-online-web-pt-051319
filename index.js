d70a0736e6320058e3e7acf70fd91c9d70c9bdd1
Kateryna Strough <katerynastrough@hotmail.com>
2019-12-22 13:24:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
